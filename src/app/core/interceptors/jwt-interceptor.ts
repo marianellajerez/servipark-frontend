97a6382e8719e79d0cc41f9f0b76064c739a286e
Marianella Jerez <marianella.08@hotmail.com>
2025-10-30 14:33:23 -0500

    Configura el interceptor de jwt

--- src/app/core/interceptors/jwt-interceptor.ts
+++ src/app/core/interceptors/jwt-interceptor.ts
@@ -1,5 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Auth } from '../services/auth';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authService = inject(Auth);
+  const token = authService.getToken();
+
+  if (token) {
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedReq);
+  }
+
   return next(req);
 };
