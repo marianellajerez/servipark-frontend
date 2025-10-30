18e8357d955dcb505a656bf4e0156b951feb4d42
Marianella Jerez <marianella.08@hotmail.com>
2025-10-30 00:32:54 -0500

    Agrega las configuraciones básicas de la aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
-import { Routes } from '@angular/router';
+// src/app/app.routes.ts
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { LoginComponent } from './public/login/login';
+import { DashboardComponent } from './features/dashboard/dashboard';
+
+import { authGuard } from './core/guards/auth-guard';
+import { roleGuard } from './core/guards/role-guard';
+import { RegistrarEntrada } from './features/tasks/registrar-entrada/registrar-entrada';
+import { GestionarUsuarios } from './features/tasks/gestionar-usuarios/gestionar-usuarios';
+
+export const routes: Routes = [
+  
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+    
+    children: [
+      {
+        path: 'registrar-entrada',
+        component: RegistrarEntrada
+      },
+      {
+        path: 'gestionar-usuarios',
+        component: GestionarUsuarios,
+        canActivate: [roleGuard],
+        data: {
+          expectedRole: 'ADMINISTRADOR'
+        }
+      },
+      { 
+        path: '', 
+        redirectTo: 'registrar-entrada', 
+        pathMatch: 'full' 
+      }
+    ]
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
